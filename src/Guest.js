72dc5d32209206b4b833a223576969940ecf4d46
sarah faust <sarah.faustmann@gmail.com>
2021-09-28 14:20:07 +0200

    Fix import in guest component

--- src/Guest.js
+++ src/Guest.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react/cjs/react.development';
+import { useEffect, useState } from 'react';
 
 export function Guest({ guest, updateAttending, deleteGuest, updateGuest }) {
   const [isInEdit, setIsInEdit] = useState(false);
